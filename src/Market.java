d806f69d29a0171018b6bd93a8e2703f6bbc657c
PrasanthSomu <prasanthsomu2000@gmail.com>
2023-12-02 20:40:24 +0530

    "constructor practice"

--- src/Market.java
+++ src/Market.java
@@ -2,21 +2,34 @@ public class Market {
 
     int price; //this is called as instance variables or global var.
     float gst;
-    String name;
+    String prod_name;
 
-    Market(int price, float gst, String name){ //local variable.
+    Market(int price, float gst, String prod_name){ //local variable.
         this.price = price;
         this.gst = gst;
-        this.name = name;
+        this.prod_name = prod_name;
+        System.out.println(prod_name+price+gst);
     }
     public static void main(String[] args) {
 
-        Market prod_1 = new Market(35,1.2f,"soap");
-        System.out.println(prod_1.name);
+        Market prod_1 = new Market(11,2,"soap");
+        prod_1.prod_name="soap";
+        prod_1.gst=2;
+        prod_1.price=10;
+        /*System.out.println(prod_1.gst);
         System.out.println(prod_1.price);
-        System.out.println(prod_1.gst);
-        //bread.price=35;
-        //bread.gst=0.4F;
+        System.out.println(prod_1.prod_name);*/
+        Market prod_2 = new Market(12,1,"oil");
+        Market prod_3 = new Market(22,1,"biscuit");
+        prod_1.sell();
+        prod_2.sell();
 
     }
+
+    public void sell(){
+        System.out.println("product name = "+prod_name);
+        System.out.println("gst = "+gst);
+        System.out.println("price = "+price);
+    }
+
 }
